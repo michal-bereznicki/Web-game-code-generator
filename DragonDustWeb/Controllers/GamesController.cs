f5e835b1184451fb9ed82166d67bae7c178cde78
Michał <mich_ber@wp.pl>
2019-01-28 14:03:29 +0100

    Basic game list page

---
+++ DragonDustWeb/Controllers/GamesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DragonDustWeb.Controllers
+{
+    public class GamesController : Controller
+    {
+        // GET: Games
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AncientTombAdventure()
+        {
+            return View();
+        }
+
+        public ActionResult KidsMusicComposer()
+        {
+            return View();
+        }
+    }
+}
