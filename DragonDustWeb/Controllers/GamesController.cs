92b61e685932e9186b26e39ace56ee4a0dd33dc2
Michał <mich_ber@wp.pl>
2019-01-28 14:23:41 +0100

    Redirecting to GameDisplayPage based on selected game

--- DragonDustWeb/Controllers/GamesController.cs
+++ DragonDustWeb/Controllers/GamesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DragonDustWeb.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,12 +17,24 @@ namespace DragonDustWeb.Controllers
 
         public ActionResult AncientTombAdventure()
         {
-            return View();
+            var viewModel = new GameDisplayViewModel
+            {
+                TitleOneliner = "Solve unique riddles in Ancient Tomb!",
+                Description = "sad",
+                GooglePlayLink = "https://play.google.com/store/apps/details?id=com.DragonDustGames.labyrinth.adventure.puzzle.riddle.ancient.tomb"
+            };
+            return View("GameDisplay",viewModel);
         }
 
         public ActionResult KidsMusicComposer()
         {
-            return View();
+            var viewModel = new GameDisplayViewModel
+            {
+                TitleOneliner = "Have fun playing realistic music instruments!",
+                Description = "opis 2",
+                GooglePlayLink = "https://play.google.com/store/apps/details?id=com.dragondustgames.kids.music.learn.composer.KidsMusicComposerFunLearn"
+            };
+            return View("GameDisplay", viewModel);
         }
     }
 }
