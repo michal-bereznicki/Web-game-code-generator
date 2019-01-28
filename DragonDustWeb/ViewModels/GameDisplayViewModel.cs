df3f35a1d91cd655b3398f2e323b990cd3d971c7
Michał <mich_ber@wp.pl>
2019-01-28 14:09:30 +0100

    GameDisplayViewModel

---
+++ DragonDustWeb/ViewModels/GameDisplayViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DragonDustWeb.ViewModels
+{
+    public class GameDisplayViewModel
+    {
+        public string TitleOneliner { get; set; }
+        public string Description { get; set; }
+
+        public string GooglePlayLink { get; set; }
+    }
+}
