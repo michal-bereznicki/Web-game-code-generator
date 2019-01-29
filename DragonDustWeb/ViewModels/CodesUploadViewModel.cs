54a080bfc6b6d4bc92811bcd67ffc7b9a2651432
Michał <mich_ber@wp.pl>
2019-01-29 17:29:47 +0100

    Codes upload viewModel

---
+++ DragonDustWeb/ViewModels/CodesUploadViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DragonDustWeb.ViewModels
+{
+    public class CodesUploadViewModel
+    {
+        public IEnumerable GameIds { get; set; }
+        public IEnumerable GameNames { get; set; }
+        public DateTime CodesExpireDate { get; set; }
+    }
+}
