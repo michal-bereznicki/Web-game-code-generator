8b67c612516ef8aa5eb0a83622d23a4299ed2001
Michał <mich_ber@wp.pl>
2019-01-29 16:44:53 +0100

    Helper readonly fields

--- DragonDustWeb/Models/Game.cs
+++ DragonDustWeb/Models/Game.cs
@@ -10,5 +10,9 @@ namespace DragonDustWeb.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string GooglePlayPageLink { get; set; }
+
+
+        public static readonly int AncientTombAdventureId = 1;
+        public static readonly int KidsMusicComposerId = 2;
     }
 }
