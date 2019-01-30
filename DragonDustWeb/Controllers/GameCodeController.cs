c1633bd3a9cc4b1789df9b75384d133c3330f745
Michał <mich_ber@wp.pl>
2019-01-30 13:20:10 +0100

    Checking if there is any code available before redirecting to email request page

--- DragonDustWeb/Controllers/GameCodeController.cs
+++ DragonDustWeb/Controllers/GameCodeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DragonDustWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,37 @@ namespace DragonDustWeb.Controllers
 {
     public class GameCodeController : Controller
     {
+        ApplicationDbContext dbContext;
+        public GameCodeController()
+        {
+            dbContext = new ApplicationDbContext();
+        }
+
         // GET: GameCode
         public ActionResult Index()
         {
             return View("GameSelection");
+        }
+
+        public ActionResult GetCode(int gameId)
+        {
+            var game = dbContext.Games.SingleOrDefault(g => g.Id == gameId);
+            if(game == null)
+                return new HttpNotFoundResult();
+
+            if(!CodeAvailable(game))
+                return View("CodeUnavailable");
+
+            return View("EmailRequest");
+        }
+
+        bool CodeAvailable(Game game)
+        {
+            return dbContext.GameCodes.Any(c =>
+                c.GameId == game.Id
+                && c.Used == false
+                && c.ExpirationDate > DateTime.Now
+            );
         }
     }
 }
