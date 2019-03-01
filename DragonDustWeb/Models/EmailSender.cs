b29b8474e0fbae923260615bca2ab054c89ed719
Michał <mich_ber@wp.pl>
2019-03-01 16:49:57 +0100

    Email sender with placeholder credentials

--- DragonDustWeb/Models/EmailSender.cs
+++ DragonDustWeb/Models/EmailSender.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Text;
 using System.Web;
 
 namespace DragonDustWeb.Models
@@ -9,7 +11,20 @@ namespace DragonDustWeb.Models
     {
         public static void SendGameCode(string adress, string gameCode)
         {
+            SmtpClient client = new SmtpClient();
+            client.Port = 587;
+            client.Host = "smtp.gmail.com";
+            client.EnableSsl = true;
+            client.Timeout = 10000;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new System.Net.NetworkCredential("user", "password");
 
+            MailMessage message = new MailMessage("user", adress, "Game code request", gameCode);
+            message.BodyEncoding = Encoding.UTF8;
+            message.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+
+            client.Send(message);
         }
     }
 }
